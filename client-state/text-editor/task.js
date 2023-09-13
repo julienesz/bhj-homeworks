efa55750552e6a6a62bcd4d460b844959d78ccc9
julia <y.andryushina@gmail.com>
2023-09-13 19:55:29 +0300

    text editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,14 +1,14 @@
 const textEditor = document.getElementById(`editor`);
-const storedText = localStorage.getItem(`text`);
-const clearButton = document.querySelector('.clear__button')
+const clearButton = document.querySelector('.clear__button');
+
+textEditor.value = localStorage.getItem(`text`);
 
 textEditor.addEventListener(`input`, () => {
     localStorage.setItem(`text`, textEditor.value);
 })
 
 clearButton.addEventListener(`click`, () => {
-    editor.value = ``;
-    localStorage.removeItem();
-  });
+    textEditor.value = ``;
+    localStorage.removeItem(`text`);
+})
   
-editor.value = localStorage.getItem(`editorValue`);
